29116fd0f88c69957fce3a503ab11b96878654a6
ivanhoe-genius <ivanhoe_genius@yahoo.com>
2023-12-22 23:31:12 +0100

    feat: introduce advanced color manipulation

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ function createGrid(numberOfDivs = 16) {
     for (let i = 0; i < numberOfDivs * numberOfDivs; i++) {
         const divElement = document.createElement('div');
         divElement.classList.add('div-element');
+        divElement.style.backgroundColor = 'black';
+        divElement.style.filter = 'brightness(1)';
         divElement.id = 'divelement' + i;
-        divElement.style.width = 500 / numberOfDivs + 'px';
+        divElement.style.width = (500 - numberOfDivs) / numberOfDivs + 'px';
 
         divContainer.appendChild(divElement);
 
         divElement.addEventListener('mouseenter', (event) => {
-            event.target.classList.add('color-change');
+            if (event.target.style.backgroundColor === 'black') {
+                event.target.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+            } else {
+                const oldFilterValue = event.target.style.filter;
+                const oldBrightnessValue = oldFilterValue.match(/\(([^)]+)\)/)[1];
+                event.target.style.filter = `brightness(${oldBrightnessValue - 0.1})`;
+
+            }
         });
     }
 }
@@ -20,14 +29,12 @@ createGrid();
 
 function enterNumber() {
     const userInput = prompt('Please enter the number of squares per side for the new grid.\nMax allowed is 100.');
-    // console.log('userInput', userInput);
 
     if (userInput === null) {
         return;
     }
 
     const convertedInput = parseInt(userInput.trim());
-    // console.log('convertedInput', convertedInput);
 
     if (!convertedInput || convertedInput < 0 || convertedInput > 100) {
         alert('Please enter a valid number of squares.\nAny value between 1 and 100.');
